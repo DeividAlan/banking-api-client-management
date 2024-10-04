11ca25d9fe3084ac4ddd10b12bd37fc5955a0239
Deivid Alan <deividalan@hotmail.com>
2024-10-03 22:57:38 -0300

    config: add server and run file

---
+++ src/main/server/server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+from src.models.sqlite.settings.connection import db_connection_handler
+
+from src.main.routes.individual_client_routes import individual_client_routes_bp
+from src.main.routes.legal_entity_client_routes import legal_entity_client_routes_bp
+
+db_connection_handler.connect_to_db()
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(individual_client_routes_bp)
+app.register_blueprint(legal_entity_client_routes_bp)
